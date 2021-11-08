440c805bf37c03abe944f01b86fd22692e3a6710
zacharyholland <90477622+zacharyholland@users.noreply.github.com>
2021-11-08 10:43:40 -0700

    new commit

--- travelpages/views.py
+++ travelpages/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def indexPageview(request) :
-    return HttpResponse('I am hungry!')
+    return render(request, 'travelpages/index.html')
 
 def aboutPageView(request) :
     return HttpResponse('I am the about page')
