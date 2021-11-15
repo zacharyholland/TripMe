77e411333ac6a75fe07415e14a0fe6fd733f386e
zacharyholland <90477622+zacharyholland@users.noreply.github.com>
2021-11-15 10:29:11 -0700

    newcommit

--- tripme/settings.py
+++ tripme/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'tripme.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backend.postgresql',
+        'NAME': 'registration',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost'
     }
 }
 
