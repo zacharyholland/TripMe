eb622cc15ae6560b1266fdc3634030a72b3c25be
zacharyholland <90477622+zacharyholland@users.noreply.github.com>
2021-11-15 10:39:53 -0700

    newcommit

--- travelpages/models.py
+++ travelpages/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Grade_Level(models.Model):
+    class_level = models.CharField(max_length=2, unique=True)
+    description = models.CharField(max_length=20)
+    
+    class Meta:
+        db_table = "grade_level"
+    
+    def __str__(self):
+        return (self.description)
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    class_level = models.ForeignKey(Grade_Level, on_delete=models.DO_NOTHING,to_field='class_level')
+
+    class Meta:
+        db_table = 'student'
+    
+    def __str__(self):
+        return (self.first_name + " " + self.last_name)
